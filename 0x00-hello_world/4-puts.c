66005382f8373e0d2a4a3c9a487a9d4e439ff6a6
nguthirumarkian~ <nguthirumarkian@gmail.com>
2022-06-02 18:15:17 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * * main -use the put function to print the string 
- * * this program prints "Programming is like building a multilingual puzzle"
- * * Return: 0
- * */
+ * main - Entry point
+ *
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
