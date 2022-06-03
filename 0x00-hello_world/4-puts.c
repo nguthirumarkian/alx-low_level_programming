9a4ebbbb5b838d306e1bfcb9b2a2ac1cf3ab8511
nguthirumarkian~ <nguthirumarkian@gmail.com>
2022-06-02 17:47:49 -0700

    my fist commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * * main -use the put function to print the string 
+ * * this program prints "Programming is like building a multilingual puzzle"
+ * * Return: 0
+ * */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
